ef2751710575a9c76591abc746ddb78cc65b72d4
ehmicky <ehmicky@gmail.com>
2022-12-11 15:34:25 +0100

    Fix linting

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -2,7 +2,7 @@ import { expectType, expectAssignable } from 'tsd'
 
 import templateName, { type Options } from 'template-name'
 
-expectType<object>(templateName(true))
+expectType<unknown>(templateName(true))
 
 templateName(true, {})
 expectAssignable<Options>({})
